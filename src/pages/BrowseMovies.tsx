9776b326f67119a7c059d45ba38e22a9c690614d
Shakhzod235 <shakhzodikramov235@gmail.com>
2025-12-03 01:38:18 +0500

    refactor: structure files into relevant folders

--- src/pages/BrowseMovies.tsx
+++ src/pages/BrowseMovies.tsx
@@ -1,7 +1,7 @@
 export const BrowseMovies = () => {
   return (
     <section className="container">
-      <h1>Browse Movies</h1>
+      <h1 className="mt-8 text-center">Browse Movies</h1>
     </section>
   );
 }
