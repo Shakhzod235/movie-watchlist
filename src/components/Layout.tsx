ad44ec24bd00b5deb83948fdfd88750ae17dba67
Shakhzod235 <shakhzodikramov235@gmail.com>
2025-12-02 23:55:16 +0500

    feat: add hover and active states for navbar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,8 +4,8 @@ export const Layout = () => {
   return (
     <>
       <header className="flex justify-center gap-16 border-b-gray-300 border-b px-8 py-4">
-        <NavLink to="/">Browse films</NavLink>
-        <NavLink to="/watchlist">Watchlist</NavLink>
+        <NavLink to="/" className="hover:border-b-2 hover:border-neutral-500">Browse films</NavLink>
+        <NavLink to="/watchlist" className="hover:border-b-2 hover:border-neutral-500">Watchlist</NavLink>
       </header>
       <main>
         <Outlet />
